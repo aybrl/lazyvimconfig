d16448439a455511badd6af29583a862da8f41c5
ayoub.berhili <ayoub.berhili@wifirst.fr>
2025-06-03 17:34:48 +0200

    resolve rebase conflicts

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,31 +4,42 @@ return {
     servers = {
       rust_analyzer = {
         settings = {
-          rust = {
-            analysis = {
-              typecheckingmode = "strict",
-            },
+          ["rust-analyzer"] = {
+            cargo = { allFeatures = true },
+            checkOnSave = { command = "clippy" },
           },
         },
       },
-      clangd = {
-        settings = {
-          clang = {
-            analysis = {
-              typecheckingmode = "strict",
-            },
-          },
-        },
-      },
+      clangd = {},
       gopls = {
         settings = {
-          go = {
-            analysis = {
-              typecheckingmode = "strict",
+          gopls = {
+            analyses = {
+              unusedparams = true,
+              shadow = true,
             },
+            staticcheck = true,
           },
         },
+      },
+      terraformls = {
+        settings = {},
       },
     },
   },
+  config = function(_, opts)
+    vim.filetype.add({
+      extension = {
+        tf = "terraform",
+        tfvars = "terraform",
+      },
+    })
+    require("lspconfig").terraformls.setup({
+      filetypes = { "terraform", "hcl", "tf", "tfvars" },
+      settings = opts.servers.terraformls.settings,
+    })
+    require("lspconfig").gopls.setup(opts.servers.gopls)
+    require("lspconfig").clangd.setup(opts.servers.clangd)
+    require("lspconfig").rust_analyzer.setup(opts.servers.rust_analyzer)
+  end,
 }
